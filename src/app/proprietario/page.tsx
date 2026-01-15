94f0331016828eac47868d59d2ec97b3e22df848
Ariele Damiani <damianiariele@gmail.com>
2026-01-15 02:58:57 +0100

    fix: corrected build errors

--- src/app/proprietario/page.tsx
+++ src/app/proprietario/page.tsx
@@ -1,4 +1,4 @@
-import { DashboardProprietarioContent } from "@/components/proprietario/DashboardProprietarioContent";
+import { DashboardProprietarioContent } from "~/components/proprietario/DashboardProprietarioContent";
 
 export default function ProprietarioDashboard() {
   return <DashboardProprietarioContent userName="Ariele Damiani" />;
