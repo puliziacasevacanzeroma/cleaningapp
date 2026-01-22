8fc9d2aed0855246d8fc155b268e8ac3843019ee
Ariele Damiani <damianiariele@gmail.com>
2026-01-22 21:04:37 +0100

    Navbar: Pulizie al centro, Prenotazioni a destra

--- src/components/proprietario/ProprietarioLayoutClient.tsx
+++ src/components/proprietario/ProprietarioLayoutClient.tsx
@@ -38,8 +38,8 @@ export function ProprietarioLayoutClient({ children, userName, userEmail, userId
   const menuItems = [
     { href: "/proprietario", label: "Dashboard", icon: "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" },
     { href: "/proprietario/proprieta", label: "Proprietà", icon: "M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" },
-    { href: "/proprietario/calendario/prenotazioni", label: "Prenotazioni", icon: "M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" },
     { href: "/proprietario/calendario/pulizie", label: "Pulizie", icon: "M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" },
+    { href: "/proprietario/calendario/prenotazioni", label: "Prenotazioni", icon: "M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" },
   ];
 
   const handleLogout = async () => {
