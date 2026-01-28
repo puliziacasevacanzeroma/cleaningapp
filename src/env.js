30415d7eaf0343cc4416da79e499c77c9c05b6fc
Ariele Damiani <damianiariele@gmail.com>
2026-01-28 20:55:22 +0100

    Make DATABASE_URL optional

--- src/env.js
+++ src/env.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().url().optional(),
     NEXTAUTH_SECRET: z.string(),
     NEXTAUTH_URL: z.preprocess(
       (str) => process.env.VERCEL_URL ?? str,
